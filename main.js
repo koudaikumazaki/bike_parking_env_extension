adcc6614c523e8dcc5257204698b830e6099d42e
koudaikumazaki <kk1974340@gmail.com>
2022-01-09 18:55:22 +0900

    wip

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 const url = location.hostname
 switch (url) {
-  case "buzzkuri.com":
+  case "bike-parking-search.com":
     const productionBodyElement = document.querySelector('html body');
     productionBodyElement.insertAdjacentHTML('afterbegin', '<p class="production env-message">現在の環境はproduction環境です。</p>');
     break;
-  case "staging.buzzkuri.com":
+  case "staging-bike-parking-search.herokuapp.com/":
     const stagingBodyElement = document.querySelector('html body');
     stagingBodyElement.insertAdjacentHTML('afterbegin', '<p class="staging env-message">現在の環境はstaging環境です。</p>');
     break;
-  case "preview.buzzkuri.com":
-    const previewBodyElement = document.querySelector('html body');
-    previewBodyElement.insertAdjacentHTML('afterbegin', '<p class="preview env-message">現在の環境はpreview環境です。</p>');
+  case "localhost:3000/":
+    const developmentBodyElement = document.querySelector('html body');
+    developmentBodyElement.insertAdjacentHTML('afterbegin', '<p class="development env-message">現在の環境はdevelopment環境です。</p>');
     break;
 }
