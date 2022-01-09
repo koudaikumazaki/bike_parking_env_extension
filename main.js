3403a5a2bcdce478a58c8c2b6848b3e68abd959e
koudaikumazaki <kk1974340@gmail.com>
2022-01-09 19:05:48 +0900

    fix: extension

--- main.js
+++ main.js
@@ -4,11 +4,11 @@ switch (url) {
     const productionBodyElement = document.querySelector('html body');
     productionBodyElement.insertAdjacentHTML('afterbegin', '<p class="production env-message">現在の環境はproduction環境です。</p>');
     break;
-  case "staging-bike-parking-search.herokuapp.com/":
+  case "staging-bike-parking-search.herokuapp.com":
     const stagingBodyElement = document.querySelector('html body');
     stagingBodyElement.insertAdjacentHTML('afterbegin', '<p class="staging env-message">現在の環境はstaging環境です。</p>');
     break;
-  case "localhost:3000/":
+  case "127.0.0.1:3000":
     const developmentBodyElement = document.querySelector('html body');
     developmentBodyElement.insertAdjacentHTML('afterbegin', '<p class="development env-message">現在の環境はdevelopment環境です。</p>');
     break;
